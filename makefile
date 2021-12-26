0cb25aaa059faa21af7a854272b52df525e372b1
hachiyuki8 <caladuil@outlook.com>
2021-12-26 17:13:02 -0500

    add startup screen

--- makefile
+++ makefile
@@ -1,10 +1,10 @@
 all: sdl
 
 sdl: sdl.o
-	clang++ -o sdl sdl.o -L/opt/homebrew/Cellar/sdl2/2.0.18/lib -L/opt/homebrew/Cellar/sdl2_image/2.0.5/lib -L/opt/homebrew/Cellar/sdl2_ttf/2.0.15/lib -lSDL2 -lSDL2_image -lSDL2_ttf
+	clang++ -o sdl sdl.o -L/opt/homebrew/Cellar/sdl2/2.0.18/lib -L/opt/homebrew/Cellar/sdl2_ttf/2.0.15/lib -lSDL2 -lSDL2_ttf
 
 sdl.o: sdl.cpp
-	clang++ -c -I/opt/homebrew/Cellar/sdl2/2.0.18/include -I/opt/homebrew/Cellar/sdl2_image/2.0.5/include -I/opt/homebrew/Cellar/sdl2_ttf/2.0.15/include sdl.cpp
+	clang++ -c -I/opt/homebrew/Cellar/sdl2/2.0.18/include/SDL2 -I/opt/homebrew/Cellar/sdl2_ttf/2.0.15/include/SDL2 sdl.cpp
 
 clean:
 	rm sdl *.o
--- sdl.cpp
+++ sdl.cpp
@@ -1,5 +1,5 @@
-
-#include <SDL2/SDL.h>
+#include <SDL.h>
+#include <SDL_ttf.h>
 #include <iostream>
 #include <stdlib.h>
 
@@ -11,9 +11,15 @@ bool loop();
 
 SDL_Window *window;
 SDL_Renderer *renderer;
-SDL_Texture *texture;
+SDL_Texture *texture, *text_texture;
+SDL_Surface *text;
+TTF_Font* font;
 SDL_Rect rect;
 Uint32 lastUpdate;
+
+
+SDL_Color text_color = { 0, 0, 0 };
+bool started = false;
 
 const int SCREEN_WIDTH = 1280;
 const int SCREEN_HEIGHT = 720;
@@ -38,14 +44,46 @@ int main(int argc, char **args)
   return 0;
 }
 
+void renderStartupText() {
+  text = TTF_RenderText_Solid(font, "Press SPACEBAR to start", text_color);
+  if (!text) {
+    cout << "Failed to render text: " << TTF_GetError() << endl;
+  }
+
+  text_texture = SDL_CreateTextureFromSurface(renderer, text);
+  SDL_Rect dest = {(SCREEN_WIDTH - text->w)/2, (SCREEN_HEIGHT - text->h)/2, text->w, text->h};
+  SDL_RenderCopy(renderer, text_texture, NULL, &dest);
+
+  SDL_DestroyTexture(text_texture);
+  SDL_FreeSurface(text);
+}
+
 bool loop() {
   SDL_Event evt;
 
   SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
   SDL_RenderClear(renderer);
 
+  if (!started) {
+    renderStartupText();
+  }
+
   Uint32 current = SDL_GetTicks();
   float dT = (current - lastUpdate) / 1000.0f;
+
+  const Uint8* keys = SDL_GetKeyboardState(NULL);
+  if (keys[SDL_SCANCODE_A]) {
+    rect.x = max(0.0f, rect.x - RECT_VELOCITY_H * dT);
+  }
+  if (keys[SDL_SCANCODE_D]) {
+    rect.x = min((float)SCREEN_WIDTH - RECT_WIDTH, rect.x + RECT_VELOCITY_H * dT);
+  }
+  if (keys[SDL_SCANCODE_W]) {
+    rect.y = max(0.0f, rect.y - RECT_VELOCITY_V * dT);
+  }
+  if (keys[SDL_SCANCODE_S]) {
+    rect.y = min((float)SCREEN_HEIGHT - RECT_HEIGHT, rect.y + RECT_VELOCITY_V * dT);
+  }
 
   while (SDL_PollEvent(&evt) != 0) {
     switch (evt.type) {
@@ -54,27 +92,15 @@ bool loop() {
       case SDL_KEYDOWN:
         switch (evt.key.keysym.sym) {
           case SDLK_SPACE:
+            started = true;
             rect.x = (SCREEN_WIDTH - RECT_WIDTH) / 2;
             rect.y = SCREEN_HEIGHT - RECT_HEIGHT;
             rect.w = RECT_WIDTH;
             rect.h = RECT_HEIGHT;
             SDL_SetTextureColorMod(texture, 255, 255, 255);
             break;
-          case SDLK_a:
-            rect.x = max(0.0f, rect.x - RECT_VELOCITY_H * dT);
-            break;
-          case SDLK_d:
-            rect.x = min((float)SCREEN_WIDTH - RECT_WIDTH, rect.x + RECT_VELOCITY_H * dT);
-            break;
-          // case SDLK_w:
-          //   rect.y = max(0.0f, rect.y - RECT_VELOCITY_V * dT);
-          //   break;
-          // case SDLK_s:
-          //   rect.y = min((float)SCREEN_HEIGHT - RECT_HEIGHT, rect.y + RECT_VELOCITY_V * dT);
-          //   break;
         }
     }
-
   }
 
   lastUpdate = current;
@@ -87,47 +113,60 @@ bool loop() {
 bool init() {
   if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
   {
-    std::cout << "Error initializing SDL: " << SDL_GetError() << std::endl;
+    cout << "Error initializing SDL: " << SDL_GetError() << endl;
     return false;
   }
 
   window = SDL_CreateWindow("Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
   if (!window)
   {
-    std::cout << "Error creating window: " << SDL_GetError() << std::endl;
+    cout << "Error creating window: " << SDL_GetError() << endl;
     return false;
   }
 
   renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
   if (!renderer)
   {
-    std::cout << "Error creating renderer: " << SDL_GetError() << std::endl;
+    cout << "Error creating renderer: " << SDL_GetError() << endl;
     return false;
   }
 
   SDL_Surface *image = SDL_LoadBMP("1.bmp");
   if (!image) {
-    std::cout << "Error loading image 1.bmp: " << SDL_GetError() << std::endl;
+    cout << "Error loading image 1.bmp: " << SDL_GetError() << endl;
     return false;
   }
 
-  // texture = SDL_CreateTextureFromSurface(renderer, image);
-  texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STATIC, 1024, 1024);
+  texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
   SDL_FreeSurface(image);
   if (!texture) {
-    std::cout << "Error creating texture: " << SDL_GetError() << std::endl;
+    cout << "Error creating texture: " << SDL_GetError() << endl;
     return false;
   }
+
+  if (TTF_Init() < 0) {
+		cout << "Error intializing SDL_ttf: " << TTF_GetError() << endl;
+		return false;
+	}
+
+  font = TTF_OpenFont("font.ttf", 48);
+	if (!font) {
+		cout << "Error loading font: " << TTF_GetError() << endl;
+		return false;
+	}
 
   SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
   SDL_RenderClear(renderer);
+
   return true;
 }
 
 void kill() {
+  TTF_CloseFont(font);
   SDL_DestroyTexture(texture);
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
 
+  TTF_Quit();
   SDL_Quit();
 }
